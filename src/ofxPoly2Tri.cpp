f41ff70aae0659f7f5a8a45f4f725ee4eb17577f
wimvanhenden <wim@littlemissrobot.com>
2015-11-04 13:56:58 +0100

    added ofVboMesh support (normals and textures as well but very basic)

--- src/ofxPoly2Tri.cpp
+++ src/ofxPoly2Tri.cpp
@@ -2,11 +2,12 @@
 //  ofxPoly2Tri.cpp
 //  poly2TriParticles
 //
-//  Created by Wim Vanhenden on 12/08/13.
+//  Created by Wim Vanhenden on 04/11/15.
 //
 //
 
 #include "ofxPoly2Tri.h"
+
 
 void ofxPoly2Tri::setHoles(vector<ofPolyline> incholes) {
     holes.clear();
@@ -19,6 +20,7 @@ void ofxPoly2Tri::setSteinerPoints(vector<ofPoint> incpoints) {
 }
 
 void ofxPoly2Tri::triangulate(ofPolyline bounds) {
+    
     vector<p2t::Point*> bound;
     vector<ofPoint>edgepoints = bounds.getVertices();
     for (int i=0;i<edgepoints.size();++i) {
@@ -38,8 +40,8 @@ void ofxPoly2Tri::triangulate(ofMesh bounds) {
 
 
 /***************************
-**** PRIVATE METHODS *******
-***************************/
+ **** PRIVATE METHODS *******
+ ***************************/
 
 void ofxPoly2Tri::doTriangulation(vector<p2t::Point*> bound) {
     CDT *cdt = new CDT(bound);
@@ -68,25 +70,90 @@ void ofxPoly2Tri::doTriangulation(vector<p2t::Point*> bound) {
     
     cdt->Triangulate();
     
-    //CREATE MESH
-    triangulatedMesh.clear();
-    triangulatedMesh.setMode(OF_PRIMITIVE_TRIANGLES);
-    for (int i = 0; i < cdt->GetTriangles().size(); i++){
-        p2t::Triangle *ortriangle = cdt->GetTriangles()[i];
+    if (bUseVboMesh) {
         
-        if (ortriangle->IsInterior()) {
-            ofColor randomColor = ofColor(ofRandom(255),ofRandom(255),ofRandom(255));
-            triangulatedMesh.addVertex(ofVec3f(ortriangle->GetPoint(0)->x,ortriangle->GetPoint(0)->y));
-            triangulatedMesh.addVertex(ofVec3f(ortriangle->GetPoint(1)->x,ortriangle->GetPoint(1)->y));
-            triangulatedMesh.addVertex(ofVec3f(ortriangle->GetPoint(2)->x,ortriangle->GetPoint(2)->y));
-            triangulatedMesh.addColor(randomColor);
-            triangulatedMesh.addColor(randomColor);
-            triangulatedMesh.addColor(randomColor);
+        vboMesh.clear();
+        
+        vboMesh.setUsage(GL_DYNAMIC_DRAW);
+        vboMesh.setMode(OF_PRIMITIVE_TRIANGLES);
+        
+        for (int i = 0; i < cdt->GetTriangles().size(); i++){
+            p2t::Triangle *ortriangle = cdt->GetTriangles()[i];
+            
+            if (ortriangle->IsInterior()) {
+                vboMesh.addVertex(ofVec3f(ortriangle->GetPoint(0)->x,ortriangle->GetPoint(0)->y,0));
+                vboMesh.addVertex(ofVec3f(ortriangle->GetPoint(1)->x,ortriangle->GetPoint(1)->y,0));
+                vboMesh.addVertex(ofVec3f(ortriangle->GetPoint(2)->x,ortriangle->GetPoint(2)->y,0));
+                
+                float val =ofRandom(220,255);
+                
+                vboMesh.addColor(ofColor(val,val,val));
+                vboMesh.addColor(ofColor(val,val,val));
+                vboMesh.addColor(ofColor(val,val,val));
+                
+                vboMesh.addNormal(ofVec3f(0,0,1));
+                vboMesh.addNormal(ofVec3f(0,0,1));
+                vboMesh.addNormal(ofVec3f(0,0,1));
+                
+                float xval = ((ortriangle->GetPoint(0)->x+scalex)/(scalex*2))*512;
+                float yval = ((ortriangle->GetPoint(0)->y+scaley)/(scaley*2))*512;
+                
+                float xvala = ((ortriangle->GetPoint(1)->x+scalex)/(scalex*2))*512;
+                float yvala = ((ortriangle->GetPoint(1)->y+scaley)/(scaley*2))*512;
+                
+                float xvalb = ((ortriangle->GetPoint(2)->x+scalex)/(scalex*2))*512;
+                float yvalb = ((ortriangle->GetPoint(2)->y+scaley)/(scaley*2))*512;
+                
+                vboMesh.addTexCoord(ofVec2f(xval,yval));
+                vboMesh.addTexCoord(ofVec2f(xvala,yvala));
+                vboMesh.addTexCoord(ofVec2f(xvalb,yvalb));
+                
+            }
         }
+        
+    } else {
+        
+        triangulatedMesh.clear();
+        
+        triangulatedMesh.setMode(OF_PRIMITIVE_TRIANGLES);
+        
+        for (int i = 0; i < cdt->GetTriangles().size(); i++){
+            p2t::Triangle *ortriangle = cdt->GetTriangles()[i];
+            
+            if (ortriangle->IsInterior()) {
+                
+                //ofColor randomColor = ofColor(ofRandom(255),ofRandom(255),ofRandom(255));
+                triangulatedMesh.addVertex(ofVec3f(ortriangle->GetPoint(0)->x,ortriangle->GetPoint(0)->y));
+                triangulatedMesh.addVertex(ofVec3f(ortriangle->GetPoint(1)->x,ortriangle->GetPoint(1)->y));
+                triangulatedMesh.addVertex(ofVec3f(ortriangle->GetPoint(2)->x,ortriangle->GetPoint(2)->y));
+                float val =ofRandom(220,255);
+                
+                triangulatedMesh.addColor(ofColor(val,val,val));
+                triangulatedMesh.addColor(ofColor(val,val,val));
+                triangulatedMesh.addColor(ofColor(val,val,val));
+                
+                triangulatedMesh.addNormal(ofVec3f(0,0,1));
+                triangulatedMesh.addNormal(ofVec3f(0,0,1));
+                triangulatedMesh.addNormal(ofVec3f(0,0,1));
+                
+                float xval = ((ortriangle->GetPoint(0)->x+scalex)/(scalex*2))*512;
+                float yval = ((ortriangle->GetPoint(0)->y+scaley)/(scaley*2))*512;
+                
+                float xvala = ((ortriangle->GetPoint(1)->x+scalex)/(scalex*2))*512;
+                float yvala = ((ortriangle->GetPoint(1)->y+scaley)/(scaley*2))*512;
+                
+                float xvalb = ((ortriangle->GetPoint(2)->x+scalex)/(scalex*2))*512;
+                float yvalb = ((ortriangle->GetPoint(2)->y+scaley)/(scaley*2))*512;
+                
+                
+                triangulatedMesh.addTexCoord(ofVec2f(xval,yval));
+                triangulatedMesh.addTexCoord(ofVec2f(xvala,yvala));
+                triangulatedMesh.addTexCoord(ofVec2f(xvalb,yvalb));
+            }
+        }
+        
+        
     }
-    
     delete cdt;
     cdt=NULL;
 }
-
-
--- src/ofxPoly2Tri.h
+++ src/ofxPoly2Tri.h
@@ -2,7 +2,7 @@
 //  ofxPoly2Tri.h
 //  poly2TriParticles
 //
-//  Created by Wim Vanhenden on 12/08/13.
+//  Created by Wim Vanhenden on 04/11/15.
 //
 //
 
@@ -13,7 +13,7 @@
 using namespace p2t;
 
 class ofxPoly2Tri {
-  
+    
 public:
     
     
@@ -24,6 +24,15 @@ public:
     
     ofMesh triangulatedMesh;
     
+    ofVboMesh vboMesh;
+    
+    bool       bUseVboMesh;
+    
+    ofPolyline currentline;
+    
+    float scalex;
+    float scaley;
+    
     
 private:
     
@@ -31,5 +40,7 @@ private:
     vector<ofPoint>    steinerpoints;
     
     void doTriangulation(vector<p2t::Point*> bound);
+    
+    
     
 };
